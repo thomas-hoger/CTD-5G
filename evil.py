9ced8bbc59e8f9324063dbbc891051eb5b089572
thoger <thomas.hoger@laas.fr>
2025-06-30 15:34:05 +0200

    arp

--- evil.py
+++ evil.py
@@ -4,7 +4,9 @@ from src.attacks.procedures import Attacks
 from src.marker.generation import AttackMarker, marker_base
 
 from scapy.all import send
-from datetime import datetime
+from src.utils.common import ip_list
+from scapy.layers.l2 import arping
+
 
 attack_id = int(sys.argv[1])
 attack_name = sys.argv[2]
@@ -12,6 +14,8 @@ attack_name = sys.argv[2]
 available_attacks = [name for name in dir(Attacks) if callable(getattr(Attacks, name)) and not name.startswith("_")]
 
 if attack_id and attack_name and attack_name in available_attacks :
+    
+    arping(ip_list["UPF"], eth="br-free5gc")
 
     # Send the first marker
     marker_start = AttackMarker(
@@ -20,7 +24,7 @@ if attack_id and attack_name and attack_name in available_attacks :
         attack_type = attack_name
     )
     send(marker_base / marker_start)
-    
+        
     # Run the attack
     attack = getattr(Attacks, attack_name)
     success = attack()
