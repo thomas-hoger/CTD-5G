0723be1ecd724e9f474817c293b062e700499502
thoger <thomashoger@hotmail.fr>
2025-07-30 19:55:08 +0200

    ngap and nas

---
+++ src/utils/protocols/nas/common.py
@@ -0,0 +1,35 @@
+
+from scapy.packet import Packet, bind_layers
+from scapy.fields import XByteField, ByteField, BitField, XNBytesField
+
+from src.utils.protocols.nas.registration import NAS_Registration
+    
+class NAS_Plaintext(Packet):
+    name = "NAS Plaintext"
+    fields_desc = [
+        XByteField("message_type", None),
+    ]
+
+class NAS_Protected(Packet):
+    name = "NAS Protected"
+    fields_desc = [
+        XNBytesField("message_auth", 0x10, 4),
+        ByteField("seq_number", 0)
+    ]
+    
+class NAS(Packet):
+    name = "NAS"
+    fields_desc = [
+        BitField("epd", None, 2),       # Extended Protocol Discriminator
+        BitField("spare", None, 6),       # 1/2 spare octet
+        BitField("security", None, 4),    # 1/2 byte : header type
+    ]
+    
+    def guess_payload_class(self, payload):
+        if self.security == 0:
+            return NAS_Plaintext
+        else:
+            return NAS_Protected
+        
+bind_layers(NAS_Plaintext, NAS_Registration, message_type=0x41)
+bind_layers(NAS_Protected, NAS)
---
+++ src/utils/protocols/nas/registration.py
@@ -0,0 +1,62 @@
+
+from scapy.packet import Packet
+from scapy.fields import ShortField, XByteField, PacketListField, X3BytesField
+from scapy.fields import BitField, XNBytesField, PacketField, XBitField
+
+class NSSAI(Packet):
+    name = "NSSAI"
+    fields_desc = [
+        XByteField("length", 4),
+        XByteField("sst", 1),
+        X3BytesField("sd", 0x010203)
+    ]
+    def extract_padding(self, p):
+        return "", p
+    
+class Requested_NSSAI(Packet):
+    name = "Requested NSSAI"
+    fields_desc = [
+        XByteField("element_id", 0x2f),
+        XByteField("length", None),
+        PacketListField("nssai_list", [], NSSAI, length_from=lambda pkt: pkt.length)
+    ]
+    def extract_padding(self, p):
+        return "", p
+    
+    
+class UE_Secu_Capabilities(Packet):
+    name = "UE Secu Capabilities"
+    fields_desc = [
+        XByteField("element_id", 0x2e),
+        XByteField("length", 0x4),
+        XNBytesField("capabilities", 0xf0f0f0f0, 4)
+    ]
+    def extract_padding(self, p):
+        return "", p
+    
+class Mobile_Identity(Packet):
+    name = "Mobile Identity"
+    fields_desc = [
+        ShortField("length", 13),
+        BitField("supi_format", 0x0, 4),
+        BitField("type_of_id", 0x0001, 4),
+        X3BytesField("mcc_and_mnc", 0x02f839), # mcc = 208 (02 + f8 -> 20 8f -> 208) mnc = 93 (39 -> 93)
+        ShortField("routing_indicator", 0),
+        XByteField("protection_scheme", 0),
+        XByteField("home_network_pub_key", 0),
+        XNBytesField("msin", 0x10, 5),
+    ]
+    def extract_padding(self, p):
+        return "", p
+    
+class NAS_Registration(Packet):
+    name = "NAS Registration"
+    fields_desc = [
+        XBitField("key_identifier", 0x0111, 4),
+        XBitField("registration_type", 0x1001, 4),
+        PacketField("mobile_identity", Mobile_Identity(), Mobile_Identity),
+        PacketField("ue_secu_capabilities", UE_Secu_Capabilities(), UE_Secu_Capabilities),
+        PacketField("requested_nssai", Requested_NSSAI(), Requested_NSSAI),
+    ]
+    def extract_padding(self, p):
+        return "", p
