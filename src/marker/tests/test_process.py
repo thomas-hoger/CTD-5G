89b1fc1d7d43d1e62d4708122bfe15629952373d
thoger <thomashoger@hotmail.fr>
2025-09-06 09:29:26 +0200

    marker fix

--- src/marker/tests/test_process.py
+++ src/marker/tests/test_process.py
@@ -17,66 +17,73 @@ CAPTURE_FILE = "./src/marker/tests/example.pcap"
 def capture():
     
     iface = "br-free5gc"
-
-    # Start tcpdump
-    tcpdump_proc = subprocess.Popen(
-        ["tcpdump", "-i", iface, "-w", CAPTURE_FILE],
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.DEVNULL,
-        preexec_fn=os.setsid
-    )
-    time.sleep(1)
-
-    # Marker start attack
-    send(marker_base/Marker(id=1, start=1, attack=1, type=b"toto"))
-
-    # Fake attack
-    send(IP(src=ip_list["EVIL"], dst=ip_list["UPF"])/UDP(dport=9999)/Raw(load="spoofed"))
     
-    # Marker start normal
-    send(marker_base/Marker(id=1, start=1, attack=0, type=b"tata"))
-    
-    # Fake normal
-    send(IP(dst=ip_list["UPF"])/UDP(dport=9999)/Raw(load="spoofed"))
+    try :
+        # Start tcpdump
+        tcpdump_proc = subprocess.Popen(
+            ["tcpdump", "-i", iface, "-w", CAPTURE_FILE],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+            preexec_fn=os.setsid
+        )
+        time.sleep(1)
 
-    # Marker stop attack
-    send(marker_base/Marker(id=1, start=0, attack=1, type=b"toto"))
-    
-    # Marker stop normal
-    send(marker_base/Marker(id=1, start=0, attack=0, type=b"tata"))
+        # Marker start attack
+        send(marker_base/Marker(id=1, start=1, attack=1, type=b"toto"))
 
-    # Stop tcpdump
-    time.sleep(1)
-    os.killpg(os.getpgid(tcpdump_proc.pid), signal.SIGINT)
-    tcpdump_proc.wait()
+        # Fake attack
+        send(IP(src=ip_list["EVIL"], dst=ip_list["UPF"])/UDP(dport=9999)/Raw(load="spoofed"))
+        
+        # Marker start normal
+        send(marker_base/Marker(id=1, start=1, attack=0, type=b"tata"))
+        
+        # Fake normal
+        send(IP(dst=ip_list["UPF"])/UDP(dport=9999)/Raw(load="spoofed"))
+
+        # Marker stop attack
+        send(marker_base/Marker(id=1, start=0, attack=1, type=b"toto"))
+        
+        # Marker stop normal
+        send(marker_base/Marker(id=1, start=0, attack=0, type=b"tata"))
+
+        # Stop tcpdump
+        time.sleep(1)
+        os.killpg(os.getpgid(tcpdump_proc.pid), signal.SIGINT)
+        tcpdump_proc.wait()
+        
+    except Exception:
+        pass
     
     yield
 
 def test_intervals():
     
     packets = rdpcap(CAPTURE_FILE)
-    for is_attack in [True, False]:
+    
+    intervals = _extract_intervals(packets)
+    attack_intervals = [x for x in intervals if x.attack]
+    benign_intervals = [x for x in intervals if not x.attack]
         
-        # 1 interval for benign and 1 for attacks
-        intervals = _extract_intervals(packets,is_attack=is_attack)
-        assert len(intervals) == 1
+    # 1 interval for benign and 1 for attacks
+    assert len(attack_intervals) == 1
+    assert len(benign_intervals) == 1
+    
+    for interval in intervals:
         
-        for interval in intervals:
-            
-            # Intervals bounds are markers
-            assert Marker in packets[interval.start] 
-            assert Marker in packets[interval.end] 
-            
-            marker_start = packets[interval.start][Marker]
-            marker_stop = packets[interval.end][Marker]
-            
-            # The first is a start and the second is an end
-            assert marker_start.start
-            assert not marker_stop.start
-            
-            # Markers are not mixed
-            assert marker_start.id == marker_stop.id 
-            assert marker_start.type == marker_stop.type 
+        # Intervals bounds are markers
+        assert Marker in packets[interval.start] 
+        assert Marker in packets[interval.end] 
+        
+        marker_start = packets[interval.start][Marker]
+        marker_stop = packets[interval.end][Marker]
+        
+        # The first is a start and the second is an end
+        assert marker_start.start
+        assert not marker_stop.start
+        
+        # Markers are not mixed
+        assert marker_start.id == marker_stop.id 
+        assert marker_start.type == marker_stop.type 
 
 def test_replace():
     
@@ -84,7 +91,8 @@ def test_replace():
     
     packets = rdpcap(CAPTURE_FILE)
 
-    intervals = _extract_intervals(packets, is_attack=True)
+    intervals = _extract_intervals(packets)
+    intervals = [x for x in intervals if x.attack]
     
     for interval in intervals:
                 
@@ -108,8 +116,9 @@ def test_process():
     ip_to_replace = ip_list["EVIL"]
     processed_packets = process(packets, ip_to_replace)
     
-    attack_intervals = _extract_intervals(packets, is_attack=True)
-    benign_intervals = _extract_intervals(packets, is_attack=False)
+    intervals = _extract_intervals(packets)
+    attack_intervals = [x for x in intervals if x.attack]
+    benign_intervals = [x for x in intervals if not x.attack]
     
     # The only missing packets are the markers
     number_of_markers = len(attack_intervals) * 2  # 2 attack markers
@@ -125,16 +134,18 @@ def test_get_packet_by_type():
     
     packets = rdpcap(CAPTURE_FILE)    
     
-    for is_attack in [True, False]:
-        packets_by_type = get_packets_by_type(packets,is_attack=is_attack)
-        assert len(packets_by_type) == 1   # benign and attack each supposed to have 1 type of pck
+    packets_by_type = get_packets_by_type(packets)
+    
+    assert len(packets_by_type) == 2  
+    
+    for type,p_list in packets_by_type.items():
         
-        for type,p_list in packets_by_type.items():
-            
-            assert len(p_list) > 0
-            
-            packets_with_evil_ip = [p for p in p_list if IP in p and (p[IP].src == ip_list["EVIL"] or p[IP].dst == ip_list["EVIL"])]
-            assert len(packets_with_evil_ip) == 0
-            
-            packets_with_markers = [p for p in p_list if Marker in p]
-            assert len(packets_with_markers) == 0
+        assert type
+        
+        assert len(p_list) > 0
+        
+        packets_with_evil_ip = [p for p in p_list if IP in p and (p[IP].src == ip_list["EVIL"] or p[IP].dst == ip_list["EVIL"])]
+        assert len(packets_with_evil_ip) == 0
+        
+        packets_with_markers = [p for p in p_list if Marker in p]
+        assert len(packets_with_markers) == 0
